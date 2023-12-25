8f676971324e3cae93ca63a789b0d305e7dbac2b
shani-allen <shani@ITs-MacBook-Pro.local>
2023-12-25 21:21:51 +0530

    add space

--- internal/data/data.go
+++ internal/data/data.go
@@ -17,20 +17,21 @@ var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
 // Data .
 type Data struct {
 	// TODO wrapped database client
-	db *gorm.DB
+	db    *gorm.DB
 	sqlDB *sql.DB
 }
 
 // NewData .
 func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
-	l:=log.NewHelper(logger)
-	var(
-		db *gorm.DB
+	l := log.NewHelper(logger)
+
+	var (
+		db    *gorm.DB
 		sqlDB *sql.DB
-		err error
+		err   error
 	)
 
-	if c.Database.Driver=="mysql"{
+	if c.Database.Driver == "mysql" {
 		dsn := fmt.Sprintf("%s?charset=utf8mb4&parseTime=True&loc=Local", c.Database.Source)
 		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{PrepareStmt: true})
 		if err != nil {
@@ -43,20 +44,20 @@ func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 			l.Errorf("Unable to get generic db interface: %v", err)
 			return nil, nil, fmt.Errorf("unable to get generic db interface: %w", err)
 		}
-				 // SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-				// sqlDB.SetMaxIdleConns(int(c.Database.MaxIdleConns))
+		// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+		// sqlDB.SetMaxIdleConns(int(c.Database.MaxIdleConns))
 
-				// // SetMaxOpenConns sets the maximum number of open connections to the database.
-				// sqlDB.SetMaxOpenConns(int(c.Database.MaxOpenConns))
-		
-				// // SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
-				// sqlDB.SetConnMaxLifetime(time.Duration(c.Database.GetMaxConnLifetimeInMins()) * time.Minute)
-	 }
+		// // SetMaxOpenConns sets the maximum number of open connections to the database.
+		// sqlDB.SetMaxOpenConns(int(c.Database.MaxOpenConns))
+
+		// // SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+		// sqlDB.SetConnMaxLifetime(time.Duration(c.Database.GetMaxConnLifetimeInMins()) * time.Minute)
+	}
 
 	d := &Data{db, sqlDB}
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
-	
+
 	return d, cleanup, nil
 }
